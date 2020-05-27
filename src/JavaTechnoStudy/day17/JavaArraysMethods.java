d6bea55ec0d4ac841f2217e92d8800cef94301f4
vivolric <62187112+vivolric@users.noreply.github.com>
2020-05-27 15:26:08 -0500

    update

--- src/JavaTechnoStudy/day17/JavaArraysMethods.java
+++ src/JavaTechnoStudy/day17/JavaArraysMethods.java
@@ -1,6 +1,7 @@
 package JavaTechnoStudy.day17;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class JavaArraysMethods {
 
@@ -13,10 +14,23 @@ public class JavaArraysMethods {
 
         //to sort array
         System.out.println("\nto sort array");
-        int[] numbers1 = {4, 1, 9, 5, 3};
+        int [] numbers1 = {4, 1, 9, 5, 3};
         System.out.println("before sort: " + Arrays.toString(numbers1));
-        Arrays.sort(numbers1);
-        System.out.println("after sort: " + Arrays.toString(numbers1));
+        // Arrays.sort(numbers1);
+        // System.out.println("after sort: " + Arrays.toString(numbers1));
+        char [] letters = {'z', 'b','f','a'};
+        System.out.println("before sort: " + Arrays.toString(letters));
+        Arrays.sort(letters);
+        System.out.println("after sort: " + Arrays.toString(letters));
+        Arrays.sort(numbers1,0,5);
+        System.out.println(Arrays.toString(numbers1));
+
+        Integer [] numbers5 ={4, 1, 9, 5, 3};  // buyukten kucuge dogru yazdirir.
+        Arrays.sort(numbers5);
+        Collections.reverse(Arrays.asList(numbers5));
+        System.out.println(Arrays.asList(numbers5));
+
+
 
         // to check if two arrays are equal
         System.out.println("\nto check if two arrays are equal");
--- src/moreAboutJava/Reversing/ReversingString.java
+++ src/moreAboutJava/Reversing/ReversingString.java
@@ -1,5 +1,7 @@
 package moreAboutJava.Reversing;
 
+import java.util.Arrays;
+
 public class ReversingString {
     public static void main(String[] args) {
 
@@ -8,15 +10,22 @@ public class ReversingString {
         int letterIndex = 0;
 
         for (int i = str.length() - 1; i >= 0; i--) {
+            // once burda kelimeyi ters ceviriyorsun, data type char oluyor
             letters[letterIndex] = str.charAt(i);
+                        // 0            n
+                        // 1            a
+                        // 2            g
+                        // 3            o
+                        // 4            d
             letterIndex++;
         }
+        System.out.println("Char Array: " + Arrays.toString(letters));
 
         String reverseStr = "";
         for (int i = 0; i < str.length(); i++) {
             reverseStr = reverseStr + letters[i];
         }
-        System.out.println(reverseStr);
+        System.out.println("String: " + reverseStr);
     }
 
 
